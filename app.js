dce629f67ba72f2f07407abb5d63b1f8cc7cc24a
kashafsaeed7842@gmail.com <kashafsaeed7842@gmail.com.noreply.github.com>
2025-12-24 14:16:58 +0500

    okk

---
+++ app.js
@@ -0,0 +1,8 @@
+const navs = document.getElementById(navlinks);
+const menuBtn = document.getElementById('menu-btn');
+
+
+menuBtn.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+
